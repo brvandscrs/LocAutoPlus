3fcbacdbcad71cb5fcbb938ec1e934773c4ff2c8
Bryan Descours <bryan.delianov@gmail.com>
2026-03-09 10:58:38 +0100

    Mise à jour du Model Vehicule.php.

--- app/Models/Vehicule.php
+++ app/Models/Vehicule.php
@@ -9,4 +9,13 @@ class Vehicule extends Model
 {
     /** @use HasFactory<\Database\Factories\VehiculeFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'immatriculation',
+        'motorisation',
+        'nb_porte',
+        'nb_place',
+        'type_boite_vitesse',
+        'prix_journalier',
+    ];
 }
