5873331990f87b80b161c27ce416feb62780962e
Bryan Descours <bryan.delianov@gmail.com>
2026-03-09 11:27:20 +0100

    Mise à jour du Seeder VehiculeSeeder.php.

--- database/seeders/VehiculeSeeder.php
+++ database/seeders/VehiculeSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class VehiculesSeeder extends Seeder
+class VehiculeSeeder extends Seeder
 {
     public function run(): void
     {
