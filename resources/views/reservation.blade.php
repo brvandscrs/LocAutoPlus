f2140f65ba4d90fc2c52b438ae2bb26e34d2ed02
Bryan Descours <bryan.delianov@gmail.com>
2026-04-20 17:06:59 +0200

    Correction des erreurs

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -6,17 +6,27 @@
 
             <div>
                 Départ :
-                <input type="date" name="datedebut" id="datedebut">
-                <input type="time" name="heuredebut" id="heuredebut">
+                <input type="date" name="datedebut" id="datedebut" value="{{ isset($datedebut) ? $datedebut : '' }}">
+                <input type="time" name="heuredebut" id="heuredebut" value="{{ isset($heuredebut) ? $heuredebut : '' }}">
             </div>
             <div>
                 Retour :
-                <input type="date" name="datefin" id="datefin">
-                <input type="time" name="heurefin" id="heurefin">
+                <input type="date" name="datefin" id="datefin" value="{{ isset($datefin) ? $datefin : '' }}">
+                <input type="time" name="heurefin" id="heurefin" value="{{ isset($heurefin) ? $heurefin : '' }}">
             </div>
         </div>
-        <div id="vehicules">
-            Liste des véhicules disponibles sera affichée ici
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6" id="vehicules">
+            @foreach ($vehicules as $vehicule)
+                <div style="border: 1px solid black; padding: 10px; margin-top: 10px;">
+                    <h3>{{ $vehicule->marque }} {{ $vehicule->modele }}</h3>
+                    <p>Motorisation : {{ $vehicule->motorisation }}</p>
+                    <p>Nombre de portes : {{ $vehicule->nb_portes }}</p>
+                    <p>Nombre de places : {{ $vehicule->nb_places }}</p>
+                    <p>Type de boîte de vitesse : {{ $vehicule->type_boite_vitesse }}</p>
+                    <p>Âge minimum requis : {{ $vehicule->age_minimum }} ans</p>
+                    <p>Prix journalier : {{ $vehicule->prix_journalier }}€</p>
+                </div>
+            @endforeach
         </div>
     </form>
 </x-guest-layout>
