d52ce23d891becfabedc991bb687b04ebba718df
Philippe Kurz <philippe.kurz@gmail.com>
2026-04-27 14:50:42 +0200

    controlleur API Contrats

--- app/Http/Controllers/Api/ContratController.php
+++ app/Http/Controllers/Api/ContratController.php
@@ -14,7 +14,6 @@ class ContratController extends Controller
      */
     public function index()
     {
-        dd('ContratController@index called'); // dump and die
         $contrats = Contrat::all();
         // return ContratResource::collection($contrats);
         return response()->json($contrats);
